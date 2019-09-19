1c7f5a1eb7c220d66d4432ce55dcba16acd96e9a
ASTR-35 <divya.prakash@ashmar.in>
2019-09-19 18:20:25 +0530

    activate library

---
+++ app/src/main/java/com/monetnetworks/mathnitishtest/MainActivity.java
@@ -0,0 +1,23 @@
+package com.monetnetworks.mathnitishtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.monetnetworks.nitishlibrary.MathCalculator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//     int multiply=   MathCalculator.Multiply(4,6);
+//        Toast.makeText(this, ""+multiply, Toast.LENGTH_SHORT).show();
+//
+//        int sum= MathCalculator.plus(4,8);
+//        Toast.makeText(this, ""+sum, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ nitishlibrary/src/main/java/com/monetnetworks/nitishlibrary/MathCalculator.java
@@ -0,0 +1,25 @@
+package com.monetnetworks.nitishlibrary;
+
+import android.widget.Toast;
+
+public class MathCalculator {
+
+        public static int plus(int a , int b)
+        {
+
+
+
+           return a+b;
+
+
+
+        }
+
+        public static int Multiply(int a, int b)
+        {
+
+            return  a*b;
+        }
+
+
+}
